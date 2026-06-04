8a9b2f38e9a593651581b324759ef8923cc460d9
cponce123.com <cponce123.com@gmail.com>
2026-06-04 16:59:51 +0000

    Fix: remove PrismaAdapter from auth (Credentials+JWT doesn't need it)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,9 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-  // @ts-ignore
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "credentials",
